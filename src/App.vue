0705f90f033e2208439ead70a7dd4ca31b8847d6
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-11 00:21:52 +0300

    workspace created

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header />
+    <div class="app-container">
+      <select>
+        <option></option>
+      </select>
+      <br />
+      <div class="slidercontainer">
+        <input class="slider" type="range" min="0.5" max="3" />
+      </div>
+      <span></span>
+      <br />
+      <textarea cols="30" rows="10"></textarea>
+      <div class="previewContainer">
+        <span> </span>
+      </div>
+      <br />
+      <button class="btn red" type="button">
+        <span>Please Read!</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <header>Text To Speech | kablosuzkedi</header>
+</template>
